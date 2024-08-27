13b97d3c8faa4ead9b2ca39ec42f9d87c6050385
jayanthvarma1 <jayanthgorinta@gmail.com>
2024-08-26 23:23:52 -0500

    Implementation of detailed view for products

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -16,6 +16,13 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {}
 
+  getProduct(theProductId: number): Observable<Product> {
+    // need to build the URL based on product Id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on category Id
 
