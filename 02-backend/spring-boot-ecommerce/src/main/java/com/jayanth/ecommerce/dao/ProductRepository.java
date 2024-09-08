81c9f906624016b4bd8f38434cc4a30c788e4a99
jayanthvarma1 <jayanthgorinta@gmail.com>
2024-09-07 23:56:52 -0500

    Backend Implementation for displaying countries and states

--- 02-backend/spring-boot-ecommerce/src/main/java/com/jayanth/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/jayanth/ecommerce/dao/ProductRepository.java
@@ -13,5 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
     Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
+
 }
  
